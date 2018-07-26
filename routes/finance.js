2f1e1a3dd7205f7dbdc59db734067a87f7e8c627
Samuel <jread912@gmail.com>
2018-07-26 16:51:58 -0700

    Updated Code To For Fetching Payments In The finance.js File

--- routes/finance.js
+++ routes/finance.js
@@ -157,6 +157,7 @@ router.get("/view/payments/:financeId", verifyToken, (req, res) => {
           };
 
           var myEnabledPayments = payments.filter(payment => payment._id === financeId);
+          console.log(myEnabledPayments)
           if(myEnabledPayments.length > 0){  
             financePayment.myEnabledPayments = myEnabledPayments;
           }          
